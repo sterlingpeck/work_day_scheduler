e9cab46e37693c18663b918b3cca7ab4596e1965
Sterling Peck <peck.sterling@gmail.com>
2022-04-15 15:38:29 -0600

    Save tasks

--- Develop/script.js
+++ Develop/script.js
@@ -4,3 +4,18 @@ $(".taskcontent").on("click", "p", function() {
   var textInput = $("<textarea>").addClass("form-control").val(content);
   $(this).replaceWith(textInput); 
 });
+
+//Saves our scheduled item
+$(".saveBtn").on("click", function() {
+  var key = $(this).prev().attr('id');
+  var value = $(this).prev().text();
+  localStorage.setItem(key, value);
+});
+
+//Replaces scheduled item
+$('.taskcontent').on('blur', 'textarea', function() {
+  var text = $(this).val().trim();
+  var newPar = $('<p>').text(text);
+  $(this).replaceWith(newPar);
+});
+
